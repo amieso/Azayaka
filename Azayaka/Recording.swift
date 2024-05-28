e6d142b9009208b065d8c4b6ee2ade18adaec80b
mpow <alex@amie.so>
2024-05-28 18:57:58 +0200

    isolate

--- Azayaka/Recording.swift
+++ Azayaka/Recording.swift
@@ -1,190 +1,270 @@
-//
-//  Recording.swift
-//  Azayaka
-//
-//  Created by Martin Persson on 2022-12-26.
-//
 
 import UserNotifications
 import ScreenCaptureKit
 import AVFAudio
-import KeyboardShortcuts
+import AVFoundation
 
-extension AppDelegate {
-    @objc func prepRecord(_ sender: NSMenuItem) {
-        switch (sender.identifier?.rawValue) {
-            case "window":  streamType = .window
-            case "display": streamType = .screen
-            case "audio":   streamType = .systemaudio
-            default: return // if we don't even know what to record I don't think we should even try
-        }
-        statusItem.menu = nil
-        updateAudioSettings()
-        // file preparation
-        screen = availableContent!.displays.first(where: { sender.title == $0.displayID.description })
-        window = availableContent!.windows.first(where: { sender.title == $0.windowID.description })
-        if streamType == .window {
-            filter = SCContentFilter(desktopIndependentWindow: window!)
-        } else {
-            let excluded = availableContent?.applications.filter { app in
-                Bundle.main.bundleIdentifier == app.bundleIdentifier && ud.bool(forKey: "hideSelf")
-            }
-            filter = SCContentFilter(display: screen ?? availableContent!.displays.first!, excludingApplications: excluded ?? [], exceptingWindows: [])
-        }
-        Task { await record(audioOnly: streamType == .systemaudio, filter: filter!) }
+enum Preferences {
+    static let fileName = "outputFileName"
+}
 
-        // while recording, keep a timer which updates the menu's stats
-        updateTimer?.invalidate()
-        updateTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-            self.updateMenu()
-        }
-        RunLoop.current.add(updateTimer!, forMode: .common) // required to have the menu update while open
-        updateTimer?.fire()
+class ScreenRecorder: NSObject, SCStreamDelegate, SCStreamOutput {
+    var vW: AVAssetWriter!
+    var awInput, micInput: AVAssetWriterInput!
+    let audioEngine = AVAudioEngine()
+    var startTime: Date?
+    var stream: SCStream!
+    var filePath: String!
+    let audioSettings: [String : Any] = [
+        AVSampleRateKey : 48000,
+        AVNumberOfChannelsKey : 2,
+        AVFormatIDKey: kAudioFormatMPEG4AAC,
+        AVEncoderBitRateKey: AudioQuality.high.rawValue * 1000
+    ]
+    var filter: SCContentFilter?
+    
+    var screen: SCDisplay?
+    var window: SCWindow?
+    var streamType: StreamType?
+    let ud = UserDefaults.standard
+    
+    override init() {
+        let userDesktop =
+        (NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true) as [String])
+            .first!
+        
+        // the `com.apple.screencapture` domain has the user set path for where they want to store screenshots or videos
+        let saveDirectory =
+        (UserDefaults(suiteName: "com.apple.screencapture")?.string(forKey: "location") ?? userDesktop)
+        as NSString
+        
+        streamType = .systemaudio
+        
+        ud.register( // default defaults (used if not set)
+            defaults: [
+                "encoder": Encoder.h264.rawValue,
+                "saveDirectory": saveDirectory,
+                Preferences.fileName: "Meeting_%t"
+            ]
+        )
+        
+        super.init()
     }
-
-    func record(audioOnly: Bool, filter: SCContentFilter) async {
+    
+    func startRecording() async {
+        let micCheck = await performMicCheck()
+        
+        if (!micCheck) { return }
+        
+        let availableContent = try! await SCShareableContent.excludingDesktopWindows(true, onScreenWindowsOnly: true)
+        
+        await record(availableContent: availableContent)
+    }
+    
+    func performMicCheck() async -> Bool {
+        if await AVCaptureDevice.requestAccess(for: .audio) { return true }
+        
+        
+        DispatchQueue.main.async {
+            let alert = NSAlert()
+            alert.messageText = "Amie needs permissions"
+            alert.informativeText = "Amie needs permission to record your microphone"
+            alert.addButton(withTitle: "Open Settings")
+            alert.addButton(withTitle: "No thanks")
+            alert.alertStyle = .warning
+            if alert.runModal() == .alertFirstButtonReturn {
+                NSWorkspace.shared.open(
+                    URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_Microphone")!
+                )
+            }
+        }
+        
+        return false
+    }
+    
+    private func record(availableContent: SCShareableContent) async {
         let conf = SCStreamConfiguration()
         conf.width = 2
         conf.height = 2
-
-        if !audioOnly {
-            conf.width = Int(filter.contentRect.width) * (ud.bool(forKey: "highRes") ? Int(filter.pointPixelScale) : 1)
-            conf.height = Int(filter.contentRect.height) * (ud.bool(forKey: "highRes") ? Int(filter.pointPixelScale) : 1)
-        }
-
-        conf.minimumFrameInterval = CMTime(value: 1, timescale: audioOnly ? CMTimeScale.max : CMTimeScale(ud.integer(forKey: "frameRate")))
-        conf.showsCursor = ud.bool(forKey: "showMouse")
+        
+        conf.minimumFrameInterval = CMTime(value: 1, timescale: CMTimeScale.max)
         conf.capturesAudio = true
         conf.sampleRate = audioSettings["AVSampleRateKey"] as! Int
         conf.channelCount = audioSettings["AVNumberOfChannelsKey"] as! Int
-
-        stream = SCStream(filter: filter, configuration: conf, delegate: self)
+        
+        stream = SCStream(filter: SCContentFilter(desktopIndependentWindow: availableContent.windows.first!), configuration: conf, delegate: self)
         do {
             try stream.addStreamOutput(self, type: .screen, sampleHandlerQueue: .global())
             try stream.addStreamOutput(self, type: .audio, sampleHandlerQueue: .global())
-            initMedia(conf: conf, audioOnly: audioOnly)
+            initMedia(conf: conf)
             try await stream.startCapture()
         } catch {
-            assertionFailure("capture failed".local)
+            assertionFailure("capture failed")
             return
         }
-
-        DispatchQueue.main.async { [self] in
-            updateIcon()
-            createMenu()
-        }
-
-        allowShortcuts(true)
+        
     }
-
-    @objc func stopRecording() {
-        statusItem.menu = nil
-
+    
+    func stopRecording() -> String {
         if stream != nil {
             stream.stopCapture()
         }
         stream = nil
         closeMedia()
         streamType = nil
-        audioFile = nil // close audio file
         window = nil
         screen = nil
         startTime = nil
-        updateTimer?.invalidate()
-
-        DispatchQueue.main.async { [self] in
-            updateIcon()
-            createMenu()
-        }
-
-        allowShortcuts(true)
-
-        let content = UNMutableNotificationContent()
-        content.title = "Recording Completed".local
-        if let filePath = filePath {
-            content.body = String(format: "File saved to: %@".local, filePath)
-        } else {
-            content.body = String(format: "File saved to folder: %@".local, ud.string(forKey: "saveDirectory")!)
-        }
-        content.sound = UNNotificationSound.default
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest(identifier: "azayaka.completed.\(Date.now)", content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error { print("Notification failed to send: \(error.localizedDescription)") }
-        }
+        
+        return filePath
     }
-
-    func updateAudioSettings() {
-        audioSettings = [AVSampleRateKey : 48000, AVNumberOfChannelsKey : 2] // reset audioSettings
-        switch ud.string(forKey: "audioFormat") {
-        case AudioFormat.aac.rawValue:
-            audioSettings[AVFormatIDKey] = kAudioFormatMPEG4AAC
-            audioSettings[AVEncoderBitRateKey] = ud.integer(forKey: "audioQuality") * 1000
-        case AudioFormat.alac.rawValue:
-            audioSettings[AVFormatIDKey] = kAudioFormatAppleLossless
-            audioSettings[AVEncoderBitDepthHintKey] = 16
-        case AudioFormat.flac.rawValue:
-            audioSettings[AVFormatIDKey] = kAudioFormatFLAC
-        case AudioFormat.opus.rawValue:
-            audioSettings[AVFormatIDKey] = ud.string(forKey: "videoFormat") != VideoFormat.mp4.rawValue ? kAudioFormatOpus : kAudioFormatMPEG4AAC
-            audioSettings[AVEncoderBitRateKey] =  ud.integer(forKey: "audioQuality") * 1000
-        default:
-            assertionFailure("unknown audio format while setting audio settings: ".local + (ud.string(forKey: "audioFormat") ?? "[no defaults]".local))
-        }
-    }
-
-    func prepareAudioRecording() {
-        var fileEnding = ud.string(forKey: "audioFormat") ?? "wat"
-        switch fileEnding { // todo: I'd like to store format info differently
-            case AudioFormat.aac.rawValue: fallthrough
-            case AudioFormat.alac.rawValue: fileEnding = "m4a"
-            case AudioFormat.flac.rawValue: fileEnding = "flac"
-            case AudioFormat.opus.rawValue: fileEnding = "ogg"
-            default: assertionFailure("loaded unknown audio format: ".local + fileEnding)
-        }
-        filePath = "\(getFilePath()).\(fileEnding)"
-        audioFile = try! AVAudioFile(forWriting: URL(fileURLWithPath: filePath), settings: audioSettings, commonFormat: .pcmFormatFloat32, interleaved: false)
-    }
-
+    
     func getFilePath() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "y-MM-dd HH.mm.ss"
         var fileName = ud.string(forKey: Preferences.fileName)
         if fileName == nil || fileName!.isEmpty {
-            fileName = "Recording at %t".local
+            fileName = "Meeting-%t"
         }
         // bit of a magic number but worst case ".flac" is 5 characters on top of this..
         let fileNameWithDates = fileName!.replacingOccurrences(of: "%t", with: dateFormatter.string(from: Date())).prefix(Int(NAME_MAX) - 5)
         return ud.string(forKey: "saveDirectory")! + "/" + fileNameWithDates
     }
-
-    func getRecordingLength() -> String {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.minute, .second]
-        formatter.zeroFormattingBehavior = .pad
-        formatter.unitsStyle = .positional
-        //if self.streamType == nil { self.startTime = nil }
-        return formatter.string(from: Date.now.timeIntervalSince(startTime ?? Date.now)) ?? "Unknown".local
-    }
-
-    func getRecordingSize() -> String {
-        do {
-            if let filePath = filePath {
-                let fileAttr = try FileManager.default.attributesOfItem(atPath: filePath)
-                let byteFormat = ByteCountFormatter()
-                byteFormat.allowedUnits = [.useMB]
-                byteFormat.countStyle = .file
-                return byteFormat.string(fromByteCount: fileAttr[FileAttributeKey.size] as! Int64)
-            }
-        } catch {
-            print(String(format: "failed to fetch file for size indicator: %@".local, error.localizedDescription))
+    
+    func initMedia(conf: SCStreamConfiguration) {
+        startTime = nil
+        
+        let filePathAndAssetWriter = getFilePathAndAssetWriter()
+        
+        filePath = filePathAndAssetWriter.0
+        vW = filePathAndAssetWriter.1
+        awInput = AVAssetWriterInput(mediaType: AVMediaType.audio, outputSettings: audioSettings)
+        micInput = AVAssetWriterInput(mediaType: AVMediaType.audio, outputSettings: audioSettings)
+        awInput.expectsMediaDataInRealTime = true
+        micInput.expectsMediaDataInRealTime = true
+        
+        if vW.canAdd(awInput) {
+            vW.add(awInput)
         }
-        return "Unknown".local
+        
+        if vW.canAdd(micInput) {
+            vW.add(micInput)
+        }
+        
+        let input = audioEngine.inputNode
+        input.installTap(onBus: 0, bufferSize: 1024, format: input.inputFormat(forBus: 0)) { [self] (buffer, time) in
+            if micInput.isReadyForMoreMediaData && startTime != nil {
+                micInput.append(buffer.asSampleBuffer!)
+            }
+        }
+        try! audioEngine.start()
+        vW.startWriting()
+    }
+    
+    func closeMedia() {
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+        awInput.markAsFinished()
+        micInput.markAsFinished()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        audioEngine.stop()
+        vW.finishWriting {
+            self.startTime = nil
+            dispatchGroup.leave()
+        }
+        dispatchGroup.wait()
+    }
+    
+    func stream(_ stream: SCStream, didOutputSampleBuffer sampleBuffer: CMSampleBuffer, of outputType: SCStreamOutputType) {
+        guard sampleBuffer.isValid else { return }
+        
+        if vW != nil && vW?.status == .writing, startTime == nil {
+            startTime = Date.now
+            vW.startSession(atSourceTime: CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
+        }
+        
+        switch outputType {
+        case .audio:
+            if awInput.isReadyForMoreMediaData {
+                awInput.append(sampleBuffer)
+            }
+        case .screen:
+            break;
+        @unknown default:
+            assertionFailure("unknown stream type")
+        }
+    }
+    
+    func stream(_ stream: SCStream, didStopWithError error: Error) { // stream error
+        print("closing stream with error:\n", error,
+              "\nthis might be due to the window closing or the user stopping from the sonoma ui")
+        DispatchQueue.main.async {
+            self.stream = nil
+            _ = self.stopRecording()
+        }
+    }
+    
+    /**
+     Get the filepath of where the asset writer is writing to and asset writer itself
+     */
+    private func getFilePathAndAssetWriter()-> (String, AVAssetWriter?){
+        let fileEnding = "m4a"
+        let fileType: AVFileType = .m4a
+        
+        filePath = "\(getFilePath()).\(fileEnding)"
+        let assetWriter = try? AVAssetWriter(outputURL: URL(fileURLWithPath: filePath), fileType: fileType)
+        
+        return (filePath, assetWriter)
     }
 }
 
-extension NSScreen {
-    var displayID: CGDirectDisplayID? {
-        return deviceDescription[NSDeviceDescriptionKey(rawValue: "NSScreenNumber")] as? CGDirectDisplayID
+// Based on https://gist.github.com/aibo-cora/c57d1a4125e145e586ecb61ebecff47c
+extension AVAudioPCMBuffer {
+    var asSampleBuffer: CMSampleBuffer? {
+        let asbd = self.format.streamDescription
+        var sampleBuffer: CMSampleBuffer? = nil
+        var format: CMFormatDescription? = nil
+        
+        guard CMAudioFormatDescriptionCreate(
+            allocator: kCFAllocatorDefault,
+            asbd: asbd,
+            layoutSize: 0,
+            layout: nil,
+            magicCookieSize: 0,
+            magicCookie: nil,
+            extensions: nil,
+            formatDescriptionOut: &format
+        ) == noErr else { return nil }
+        
+        var timing = CMSampleTimingInfo(
+            duration: CMTime(value: 1, timescale: Int32(asbd.pointee.mSampleRate)),
+            presentationTimeStamp: CMClockGetTime(CMClockGetHostTimeClock()),
+            decodeTimeStamp: .invalid
+        )
+        
+        guard CMSampleBufferCreate(
+            allocator: kCFAllocatorDefault,
+            dataBuffer: nil,
+            dataReady: false,
+            makeDataReadyCallback: nil,
+            refcon: nil,
+            formatDescription: format,
+            sampleCount: CMItemCount(self.frameLength),
+            sampleTimingEntryCount: 1,
+            sampleTimingArray: &timing,
+            sampleSizeEntryCount: 0,
+            sampleSizeArray: nil,
+            sampleBufferOut: &sampleBuffer
+        ) == noErr else { return nil }
+        
+        guard CMSampleBufferSetDataBufferFromAudioBufferList(
+            sampleBuffer!,
+            blockBufferAllocator: kCFAllocatorDefault,
+            blockBufferMemoryAllocator: kCFAllocatorDefault,
+            flags: 0,
+            bufferList: self.mutableAudioBufferList
+        ) == noErr else { return nil }
+        
+        return sampleBuffer
     }
 }
